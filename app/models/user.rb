abf73d1f9e8068075803365c8379ed7eaa10a7c3
pollockcl <pollock.clee@gmail.com>
2018-03-30 16:47:22 -0600

    validated password and added has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   validates :name, presence: true
+  validates :password, presence: true
   has_many :user_playlists
   has_many :playlists, through: :user_playlists
+
+  has_secure_password
+
 end
