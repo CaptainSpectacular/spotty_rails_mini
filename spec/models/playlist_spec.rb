5a27a77e62bed1b0cf36bcf6a70b421d0145170a
pollockcl <pollock.clee@gmail.com>
2018-03-29 18:42:58 -0600

    added tests for relationship between song and playlist

--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Playlist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'must have a name' do
+      playlist = Playlist.new()
+
+      expect(playlist).to_not be_valid
+    end
+
+    it 'is valid with all attributes' do
+      playlist = Playlist.new(name: 'Road')
+
+      expect(playlist).to be_valid
+    end
+  end
+
+  describe 'relationships' do
+    it 'can have many songs' do
+      playlist = Playlist.new(name: 'Road')
+      song1 = Song.new(name: 'On the Road Again')
+      song2 = Song.new(name: 'Hit the Road Jack')
+      playlist << song1
+      playlist << song2
+
+      expect(playlist).to respond_to(:songs)
+      expect(playlist.songs).to eq([song1, song2])
+    end
+  end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -13,6 +13,12 @@ RSpec.describe Song, type: :model do
 
       expect(song).to_not be_valid
     end
+
+    it 'is valid with all attributes' do
+      song = Song.new(name: 'all star')
+
+      expect(song).to be_valid
+    end
   end
 
   describe 'relationships' do
@@ -21,6 +27,18 @@ RSpec.describe Song, type: :model do
       song = album.songs.new(name: 'all star')
 
       expect(song.album.name).to eq('spaceloung')
+    end
+
+    it 'has many playlists' do
+      playlist1 = Playlist.new(name: 'Road')
+      playlist2 = Playlist.new(name: 'Lounge')
+      song = Song.new(name: 'all star')
+
+      playlist1 << song
+      playlist << song
+
+      expect(song).to respond_to(:playlists)
+      expect(song.playlists).to eq(playlist1, playlist2)
     end
   end
 end
