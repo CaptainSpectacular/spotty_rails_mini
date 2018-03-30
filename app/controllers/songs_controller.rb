9caa04dcf065176dec75ea4bea07cec9096928bc
pollockcl <pollock.clee@gmail.com>
2018-03-30 06:51:08 -0600

    added method for show page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,4 +2,8 @@ class SongsController < ApplicationController
   def index
     render locals: { songs: Song.all }
   end
+
+  def show
+    render locals: { song: Song.find(params[:id]) }
+  end
 end
