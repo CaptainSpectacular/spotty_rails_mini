7b11ba6dae34e92b9d388575838b5d5d35f531cf
pollockcl <pollock.clee@gmail.com>
2018-03-29 18:23:10 -0600

    added tests for model validations and relationships

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Album, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Album, type: :album do
+  describe 'validations' do
+    it 'must have a name' do
+      album = Album.create()
+
+      expect(album).to_not be_valid
+    end
+
+    it 'is valid with all attributes' do
+      album = Album.new(name: 'spacelounge')
+
+      expect(album).to be_valid
+    end
+  end
+
+  describe 'relationships' do
+    it 'has many songs' do
+      album = Album.new(name: 'spacelounge')
+      song1 = album.songs.new(name: 'all star')
+      song2 = album.songs.new(name: 'some BODY')
+
+      expect(album).to respond_to(:songs)
+      expect(album.songs.first.name).to eq(song1.name)
+      expect(album.songs.last.name).to eq(song2.name)
+    end
+  end
 end
