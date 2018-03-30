c116b03d356020da0386e0734f8f3f9c12f53c1e
pollockcl <pollock.clee@gmail.com>
2018-03-29 18:30:00 -0600

    added validation and relationship tests

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'must have a name' do
+      song = Song.new()
+
+      expect(song).to_not be_valid
+    end
+
+    it 'must have an album' do
+      song = Song.new(name: 'all star')
+
+      expect(song).to_not be_valid
+    end
+  end
+
+  describe 'relationships' do
+    it 'belongs to album' do
+      album = Album.new(name:'spaceloung')
+      song = album.songs.new(name: 'all star')
+
+      expect(song.album.name).to eq('spaceloung')
+    end
+  end
 end
