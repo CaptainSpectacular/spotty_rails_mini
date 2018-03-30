2dee9c093c1c315dc53577e05459b8b0d43a22f3
pollockcl <pollock.clee@gmail.com>
2018-03-30 08:09:53 -0600

    added model tests for user to playlists relationship ;

--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -3,13 +3,23 @@ require 'rails_helper'
 RSpec.describe Playlist, type: :model do
   describe 'validations' do
     it 'must have a name' do
-      playlist = Playlist.new()
+      user = User.create(name: 'Bennet Foddley')
+      playlist = user.playlists.new()
+
+      expect(playlist).to_not be_valid
+    end
+
+    # TODO validate that must have at least 1 user.
+    xit 'must belong to a user' do
+      playlist = Playlist.create(name: 'Road')
 
       expect(playlist).to_not be_valid
     end
 
     it 'is valid with all attributes' do
-      playlist = Playlist.new(name: 'Road')
+      user = User.create(name: 'Bennet Foddley')
+      playlist = user.playlists.create(name: 'Road')
+
 
       expect(playlist).to be_valid
     end
@@ -27,7 +37,24 @@ RSpec.describe Playlist, type: :model do
       expect(playlist.songs).to eq([song1, song2])
     end
 
-    scenario 'cannot have duplicate songs' do
+    it 'has many users' do
+      playlist = Playlist.create(name: 'Road')
+      user1 = User.create(name: 'Bennet Foddley')
+      user2 = User.create(name: 'Nobunaga')
+      user3 = User.create(name: 'Phinks')
+
+
+      user1.playlists << playlist
+      user2.playlists << playlist
+      user3.playlists << playlist
+
+      expect(playlist).to respond_to(:users)
+      expect(playlist.users.first.name).to eq(user1.name)
+      expect(playlist.users.last.name).to eq(user3.name)
+      expect(playlist.users.size).to eq(3)
+    end
+
+    it 'cannot have duplicate songs' do
       album1 = Album.create(name: 'spacelounge')
       album2 = Album.create(name: 'dark souls ost')
       song1 = Song.create(name: 'On the Road Again', album_id: album1.id)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'must have a name' do
+      user = User.create()
+
+      expect(user).to_not be_valid
+    end
+
+    it 'is valid with all attributes' do
+      user = User.create(name: 'Bennet Foddley')
+
+      expect(user).to be_valid
+    end
+  end
+
+  describe 'traits' do
+    it 'is not an admin by default' do
+      user = User.create(name: 'Bennet Foddley')
+
+      expect(user.is_admin).to eq(false)
+    end
+
+    it 'can be an admin' do
+      user = User.create(name: 'Bennet Foddley', is_admin: true)
+
+      expect(user.is_admin).to eq(true)
+    end
+  end
+
+  describe 'relationships' do
+    it 'can have many playlists' do
+      playlist1 = Playlist.create(name: 'Road')
+      playlist2 = Playlist.create(name: 'Workout')
+      user = User.create(name: 'Bennet Foddley')
+
+      user.playlists << playlist1
+      user.playlists << playlist2
+
+      expect(user.playlists.size).to eq(2)
+      expect(user.playlists.first.name).to eq(playlist1.name)
+      expect(user.playlists.last.name).to eq(playlist2.name)
+    end
+  end
 end
