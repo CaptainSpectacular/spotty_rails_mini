6d8432bc5189d58995d6c44d167426da665e5c7b
pollockcl <pollock.clee@gmail.com>
2018-03-30 19:18:50 -0600

    added some routes for logging in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'sessions/new'
+
+  resources :users
   resources :albums
   resources :songs
+
+  get '/sign_up',   to: 'users#new'
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
