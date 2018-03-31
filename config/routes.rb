e4bed49d91536054e0db2ea694edb9eab7091854
pollockcl <pollock.clee@gmail.com>
2018-03-31 15:53:51 -0600

    all tests passing. Fixed a routing bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+
+  root to: 'sessions#new'
+
   get 'users/show'
 
   get 'users/new'
@@ -11,9 +13,10 @@ Rails.application.routes.draw do
 
   resources :albums
   resources :songs
+  resources :users, exclude: %i[create new]
 
   get '/sign_up',   to: 'users#new'
-  post '/sign_up',  to: 'users#new'
+  post '/sign_up',  to: 'users#create'
   get '/login',     to: 'sessions#new'
   post '/login',    to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
