437069b7ab4afefe909ae09606f2dda793cfaed2
pollockcl <pollock.clee@gmail.com>
2018-04-01 12:08:13 -0600

    some style fixes and cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   get '/login',     to: 'sessions#new'
   post '/login',    to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  patch '/add', to: 'playlists#update'
+  patch '/add',     to: 'playlists#update'
 end
