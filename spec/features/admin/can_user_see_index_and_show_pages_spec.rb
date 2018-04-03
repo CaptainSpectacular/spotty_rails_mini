7e8fe48a625922a15e9c9d3fa30d8c5a8f66cdc7
pollockcl <pollock.clee@gmail.com>
2018-04-03 12:07:07 -0600

    added tests for an admin to view user index and show pages

---
+++ spec/features/admin/can_user_see_index_and_show_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+context 'an admin' do
+
+  before(:each) do
+    @admin = User.create(username: 'admin', password: 'admin', role: 1)
+    allow_any_instance_of(Admin::BaseController).to receive(:current_user).and_return(@admin)
+  end
+
+  context 'visits the user index page' do
+    scenario 'they can see all users' do
+      user1 = User.create(username: 'TheRealIanDouglas', password: 'supermut')
+      user2 = User.create(username: 'lluthor', password: 'password')
+
+      visit admin_users_path
+
+      expect(page).to have_content(@admin.username)
+      expect(page).to have_content(@admin.role)
+      expect(page).to have_content(user1.username)
+      expect(page).to have_content(user1.role)
+      expect(page).to have_content(user2.username)
+      expect(page).to have_content(user2.role)
+    end
+  end
+
+  context 'visits a user show page' do
+    scenario 'they can see information and modification' do
+      user = User.create(username: 'TheRealIanDouglas', password: 'supermut')
+
+      visit admin_user_path(user)
+
+      expect(page).to have_content(user.username)
+      expect(page).to have_content(user.role)
+      expect(page).to have_content(user.created_at)
+      expect(page).to have_content('Delete')
+      expect(page).to have_content('Promote')
+    end
+  end
+end
